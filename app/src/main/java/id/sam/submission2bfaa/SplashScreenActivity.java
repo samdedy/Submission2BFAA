6154a21d9012a080092e09fffe4551c9bdf39cc3
samga <samgalaxy111111@gmail.com>
2020-09-12 21:25:28 +0700

    Splash Screen

---
+++ app/src/main/java/id/sam/submission2bfaa/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package id.sam.submission2bfaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+    }
+}
